9c6925dd1db3222dbf6f2cad8fa0b651c35a9b8e
Alea Chlodnik <achlodnik@gmail.com>
2024-04-09 13:10:50 +0200

    [ADD] usage message and basic input checker

--- my_teams_cli/include/myteams_cli.h
+++ my_teams_cli/include/myteams_cli.h
@@ -7,6 +7,10 @@
 
 #ifndef MYTEAMS_CLI_H_
     #define MYTEAMS_CLI_H_
+
+    #include <stdio.h>
     #include "../../include/myteams.h"
+
 int myteams_cli(int argc, char **argv);
+
 #endif /* !MYTEAMS_CLI_H_ */
--- my_teams_cli/src/main.c
+++ my_teams_cli/src/main.c
@@ -7,7 +7,43 @@
 
 #include "myteams_cli.h"
 
+int check_input(char *ip, char *port)
+{
+    if (ip == NULL || port == NULL)
+        return -1;
+    for (int i = 0; ip[i] != '\0'; i++) {
+        if (ip[i] < '0' || ip[i] > '9' || ip[i] != '.') {
+            perror("Error: ip is invalid\n");
+            return -1;
+        }
+    }
+    for (int i = 0; port[i] != '\0'; i++) {
+        if (port[i] < '0' || port[i] > '9') {
+            perror("Error: port must be a number\n");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+void display_usage(void)
+{
+    printf("USAGE: ./myteams_cli ip port\n\tip\tis the server ip"
+        " address on which the server socket listens\n\tport\t is"
+        " the port number on which the server socket listens\n");
+}
+
 int main(int const argc, char *argv[])
 {
-    return myteams_cli(argc, argv);
+    if (argc < 1 || argc > 2)
+        return 84;
+    if (argc == 1 && strcmp(argv[1], "-help") == 0) {
+        display_usage();
+        return 84;
+    }
+    if (checkInput(argv[1], argv[2]) == -1)
+        return 84;
+    if (myteams_cli(argc, argv) == -1)
+        return 84;
+    return 0;
 }
