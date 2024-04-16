47ba40671eb913ea7e84df9e643a4251a4877482
Alea Chlodnik <achlodnik@gmail.com>
2024-04-16 18:33:28 +0200

    [PH]

--- my_teams_cli/src/tools.c
+++ my_teams_cli/src/tools.c
@@ -7,6 +7,16 @@
 
 #include "myteams_cli.h"
 
+char *get_msg_after_status(char *server_msg)
+{
+    int len = strlen(server_msg);
+    for (int i = 0; i < len - 1; i++) {
+        server_msg[i] = server_msg[i + 1];
+    }
+    server_msg[len - 1] = '\0';
+    return server_msg;
+}
+
 void do_multiple_frees(char *one, char *two, char *three, char *four)
 {
     if (one != NULL)
