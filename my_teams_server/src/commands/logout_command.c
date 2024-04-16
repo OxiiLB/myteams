6d08515650118f1ccee04a4e48646079edb7a548
OxiiLB <romain.giraud@epitech.eu>
2024-04-16 15:52:15 +0200

    [UPDATE] logout and users

--- my_teams_server/src/commands/logout_command.c
+++ my_teams_server/src/commands/logout_command.c
@@ -11,8 +11,21 @@
 void logout_command(my_teams_server_struct_t *my_teams_server_struct,
     char __attribute__((unused)) * command)
 {
-    server_event_user_logged_out(my_teams_server_struct->clients[my_teams_server_struct->actual_sockfd].user->uuid);
-    my_teams_server_struct->clients[my_teams_server_struct->actual_sockfd].user = NULL;
+    server_event_user_logged_out(my_teams_server_struct->clients
+    [my_teams_server_struct->actual_sockfd].user->uuid);
+    my_teams_server_struct->clients[my_teams_server_struct->
+    actual_sockfd].is_logged = false;
+    if (my_teams_server_struct->clients[my_teams_server_struct->
+    actual_sockfd].buffer.input_buffer)
+        free(my_teams_server_struct->clients
+        [my_teams_server_struct->actual_sockfd].buffer.input_buffer);
+    if (my_teams_server_struct->clients
+    [my_teams_server_struct->actual_sockfd].buffer.output_buffer)
+        free(my_teams_server_struct->clients[my_teams_server_struct->
+        actual_sockfd].buffer.output_buffer);
+    my_teams_server_struct->clients[my_teams_server_struct->
+    actual_sockfd].user = NULL;
     close(my_teams_server_struct->actual_sockfd);
-    FD_CLR(my_teams_server_struct->actual_sockfd, &my_teams_server_struct->fd.save_input);
+    FD_CLR(my_teams_server_struct->actual_sockfd,
+    &my_teams_server_struct->fd.save_input);
 }
