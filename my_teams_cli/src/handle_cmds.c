f403a81047f3e941163838e68b50e2794dd0438c
Alea Chlodnik <achlodnik@gmail.com>
2024-04-18 15:59:27 +0200

    [PH]

--- my_teams_cli/src/handle_cmds.c
+++ my_teams_cli/src/handle_cmds.c
@@ -42,19 +42,15 @@ void handle_users(char **info)
         user_uuid = get_msg_up_to_char(info[i], '\a', 2);
         user_name = get_msg_after_nb(info[i], (int)strlen(user_uuid) + 2);
         client_print_users(user_uuid, user_name, user_status);
-        printf("\n");
         do_multiple_frees(user_uuid, user_name, NULL, NULL);
     }
     printf("\n");
-
 }
 
 void handle_user(char **info)
 {
-    int user_status = 0;
+    print_2d_array(info, 0); //////////////////////////////
+    exit(0); /////////////////////////////////////////////
 
-    user_status = atoi(info[1]);
-    printf("%s", info[4]);
-    client_print_user(info[2], info[3], user_status);
-    printf("\n");
+    //client_print_user(user_uuid, user_name, user_status);
 }
