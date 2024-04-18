37b99ff0be6841f3bdea5a3ec6e57ef029a81857
Tom Mendy <tom.mendy@epitech.eu>
2024-04-18 12:27:18 +0200

    [FIX] logout

--- my_teams_server/src/commands/logout_command.c
+++ my_teams_server/src/commands/logout_command.c
@@ -11,11 +11,11 @@
 void logout_command(teams_server_t *teams_server, char *command)
 {
     if (teams_server->clients[teams_server->actual_sockfd].user == NULL) {
-        dprintf(teams_server->actual_sockfd, "500|Not logged in");
-        dprintf(teams_server->actual_sockfd, SPLITTER_STR);
+        dprintf(teams_server->actual_sockfd, "502|Unauthorized action\n%s",
+            SPLITTER_STR);
         return;
     }
-    dprintf(teams_server->actual_sockfd, "200|/logout\n%s\n%s\n",
+    dprintf(teams_server->actual_sockfd, "200|/logout\n%s\b%s\n",
         teams_server->clients[teams_server->actual_sockfd].user->uuid,
         teams_server->clients[teams_server->actual_sockfd].user->username);
     dprintf(teams_server->actual_sockfd, SPLITTER_STR);
--- my_teams_server/src/commands/users_command.c
+++ my_teams_server/src/commands/users_command.c
@@ -8,20 +8,31 @@
 
 #include "myteams_server.h"
 
+int check_errors(teams_server_t *teams_server, char *command)
+{
+    if (!command)
+        return 1;
+    if (teams_server->clients[teams_server->actual_sockfd].user == NULL) {
+        dprintf(teams_server->actual_sockfd, "502|Unauthorized action\n%s",
+            SPLITTER_STR);
+        return 1;
+    }
+    if (strlen(command) != 0) {
+        dprintf(teams_server->actual_sockfd, "500|Internal Server Error\n%s",
+            SPLITTER_STR);
+        return 1;
+    }
+    return 0;
+}
+
 void users_command(teams_server_t *teams_server, char *command)
 {
     user_t *user = NULL;
 
-    if (!command)
+    if (check_errors(teams_server, command) == 1)
         return;
-    if (strlen(command) != 0) {
-        dprintf(teams_server->actual_sockfd, "500|Internal Server Error\n");
-        dprintf(teams_server->actual_sockfd, SPLITTER_STR);
-        return;
-    }
     dprintf(teams_server->actual_sockfd, "200|/users\n");
-    TAILQ_FOREACH(user, &teams_server->all_user, next)
-    {
+    TAILQ_FOREACH(user, &teams_server->all_user, next) {
         dprintf(teams_server->actual_sockfd, "%s\b%s\b",
             user->uuid, user->username);
         if (user->nb_clients > 0)
