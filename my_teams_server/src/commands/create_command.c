28eaae1257068d406afc77c4fd675ce5d3640bcd
Tom Mendy <tom.mendy@epitech.eu>
2024-04-18 11:25:30 +0200

    [FIX] coding-style

--- my_teams_server/src/commands/create_command.c
+++ my_teams_server/src/commands/create_command.c
@@ -7,109 +7,87 @@
 
 #include "myteams_server.h"
 
-static int find_team(teams_server_t *teams_server, team_t *team)
+static int add_team(teams_server_t *teams_server, char **command_line,
+    int nb_args, all_context_t *all_context)
 {
-    if (teams_server->clients[teams_server->actual_sockfd].user->
-        team_context[0] != '\0') {
-        team = search_in_teams(&teams_server->all_teams, teams_server->
-        clients[teams_server->actual_sockfd].user->team_context);
-        if (team == NULL) {
-            dprintf(teams_server->actual_sockfd, "500|You need to be in a team\n");
-            dprintf(teams_server->actual_sockfd, SPLITTER_STR);
-            return KO;
-        }
-    }
-    return OK;
-}
+    team_t *new_team = NULL;
 
-static int find_channel(teams_server_t *teams_server, team_t *team, channel_t *channel)
-{
-    if (teams_server->clients[teams_server->actual_sockfd].user->
-        channel_context[0] != '\0') {
-        channel = search_in_channels(&team->channels_head, teams_server->
-        clients[teams_server->actual_sockfd].user->channel_context);
-        if (channel == NULL) {
-            dprintf(teams_server->actual_sockfd, "500|You need to be in a channel\n");
-            dprintf(teams_server->actual_sockfd, SPLITTER_STR);
-            return KO;
-        }
-    }
-    return OK;
-}
-
-static int find_thread(teams_server_t *teams_server, channel_t *channel, thread_t *thread)
-{
-    if (teams_server->clients[teams_server->actual_sockfd].user->
-        thread_context[0] != '\0') {
-        thread = search_in_threads(&channel->threads_head, teams_server->
-        clients[teams_server->actual_sockfd].user->thread_context);
-        if (thread == NULL) {
-            dprintf(teams_server->actual_sockfd, "500|You need to be in a thread\n");
-            dprintf(teams_server->actual_sockfd, SPLITTER_STR);
-            return KO;
-        }
-    }
-    return OK;
-}
-
-int add_team(teams_server_t *teams_server, char **command_line, int nb_args, team_t *team)
-{
-    if (team == NULL) {
+    if (all_context->team == NULL) {
         if (4 != nb_args) {
-            dprintf(teams_server->actual_sockfd, "500|You need to be in a team\n");
+            dprintf(teams_server->actual_sockfd, "500|azea\n");
             return KO;
         }
-        team_t *new_team = calloc(sizeof(team_t), 1);
+        new_team = calloc(sizeof(team_t), 1);
         TAILQ_INIT(&(new_team->channels_head));
         strcpy(new_team->team_name, command_line[1]);
         strcpy(new_team->team_desc, command_line[3]);
         generate_random_uuid(new_team->team_uuid);
         TAILQ_INSERT_TAIL(&(teams_server->all_teams), new_team, next);
         dprintf(teams_server->actual_sockfd, "200|Team created\n");
+        dprintf(teams_server->actual_sockfd, new_team->team_uuid);
+        dprintf(teams_server->actual_sockfd, "\b");
+        dprintf(teams_server->actual_sockfd, new_team->team_name);
+        dprintf(teams_server->actual_sockfd, "\b");
+        dprintf(teams_server->actual_sockfd, new_team->team_desc);
+        dprintf(teams_server->actual_sockfd, "\n");
         dprintf(teams_server->actual_sockfd, SPLITTER_STR);
         return OK;
     }
     return OK;
 }
 
-int add_channel(teams_server_t *teams_server, char **command_line, int nb_args, team_t *team, channel_t *channel)
+static int add_channel(teams_server_t *teams_server, char **command_line,
+    int nb_args, all_context_t *all_context)
 {
-    if (channel == NULL) {
+    channel_t *new_channel = NULL;
+
+    if (all_context->channel == NULL) {
         if (4 != nb_args) {
-            dprintf(teams_server->actual_sockfd, "500|You need to be in a channel\n");
+            dprintf(teams_server->actual_sockfd, "500|channel\n");
             return KO;
         }
-        channel_t *new_channel = calloc(sizeof(channel_t), 1);
+        new_channel = calloc(sizeof(channel_t), 1);
         TAILQ_INIT(&(new_channel->threads_head));
         strcpy(new_channel->channel_name, command_line[1]);
         strcpy(new_channel->channel_desc, command_line[3]);
         generate_random_uuid(new_channel->channel_uuid);
-        TAILQ_INSERT_TAIL(&(team->channels_head), new_channel, next);
+        TAILQ_INSERT_TAIL(&(all_context->team->channels_head), new_channel,
+            next);
         dprintf(teams_server->actual_sockfd, "200|Channel created\n");
+        dprintf(teams_server->actual_sockfd, new_channel->channel_name);
+        dprintf(teams_server->actual_sockfd, "\b");
+        dprintf(teams_server->actual_sockfd, new_channel->channel_name);
+        dprintf(teams_server->actual_sockfd, "\b");
+        dprintf(teams_server->actual_sockfd, new_channel->channel_desc);
+        dprintf(teams_server->actual_sockfd, "\n");
         dprintf(teams_server->actual_sockfd, SPLITTER_STR);
         return OK;
     }
     return OK;
 }
 
-int add_thread(teams_server_t *teams_server, char **command_line, int nb_args, channel_t *channel, thread_t *thread)
+static int add_thread(teams_server_t *teams_server, char **command_line,
+    int nb_args, all_context_t *all_context)
 {
-    if (thread == NULL) {
+    thread_t *new_thread = NULL;
+
+    if (all_context->thread == NULL) {
         if (4 != nb_args) {
-            dprintf(teams_server->actual_sockfd, "500|no hread\n");
+            dprintf(teams_server->actual_sockfd, "500|no thread\n");
             return KO;
         }
-        thread_t *new_thread = calloc(sizeof(thread_t), 1);
+        new_thread = calloc(sizeof(thread_t), 1);
         strcpy(new_thread->thread_name, command_line[1]);
         strcpy(new_thread->thread_desc, command_line[3]);
         generate_random_uuid(new_thread->thread_uuid);
-        TAILQ_INSERT_TAIL(&(channel->threads_head), new_thread, next);
+        TAILQ_INSERT_TAIL(&(all_context->channel->threads_head), new_thread,
+            next);
         dprintf(teams_server->actual_sockfd, "200|");
-        dprintf(teams_server->actual_sockfd, thread->thread_uuid);
+        dprintf(teams_server->actual_sockfd, new_thread->thread_uuid);
         dprintf(teams_server->actual_sockfd, "\b");
-        dprintf(teams_server->actual_sockfd, thread->thread_name);
+        dprintf(teams_server->actual_sockfd, new_thread->thread_name);
         dprintf(teams_server->actual_sockfd, "\b");
-        dprintf(teams_server->actual_sockfd, thread->thread_desc);
+        dprintf(teams_server->actual_sockfd, new_thread->thread_desc);
         dprintf(teams_server->actual_sockfd, "\n");
         dprintf(teams_server->actual_sockfd, SPLITTER_STR);
         return OK;
@@ -118,52 +96,41 @@ int add_thread(teams_server_t *teams_server, char **command_line, int nb_args, c
 }
 
 int add_message(teams_server_t *teams_server, char **command_line,
-    int nb_args, thread_t *thread)
+    int nb_args, all_context_t *all_context)
 {
+    message_t *new_message = NULL;
+
     if (2 != nb_args) {
         dprintf(teams_server->actual_sockfd, "500|no thread\n");
         return KO;
     }
-    message_t *new_message = calloc(sizeof(message_t), 1);
+    new_message = calloc(sizeof(message_t), 1);
     strcpy(new_message->text, command_line[1]);
     generate_random_uuid(new_message->message_uuid);
-    TAILQ_INSERT_TAIL(&(thread->messages_head), new_message, next);
+    TAILQ_INSERT_TAIL(&(all_context->thread->messages_head), new_message,
+        next);
     dprintf(teams_server->actual_sockfd, "200|Thread created\n");
     dprintf(teams_server->actual_sockfd, SPLITTER_STR);
     return OK;
 }
 
-int find_all(teams_server_t *teams_server, team_t *team, channel_t *channel,
-    thread_t *thread)
+int add_all(teams_server_t *teams_server, char **command_line,
+    int nb_args, all_context_t *create)
 {
-    if (find_team(teams_server, team) == KO)
+    if (add_team(teams_server, command_line, nb_args, create) == KO)
         return KO;
-    if (find_channel(teams_server, team, channel) == KO)
+    if (add_channel(teams_server, command_line, nb_args, create) == KO)
         return KO;
-    if (find_thread(teams_server, channel, thread) == KO)
+    if (add_thread(teams_server, command_line, nb_args, create) == KO)
         return KO;
-    return OK;
-}
-
-int add_all(teams_server_t *teams_server, char **command_line, int nb_args,
-    team_t *team, channel_t *channel, thread_t *thread)
-{
-    if (add_team(teams_server, command_line, nb_args, team) == KO)
-        return KO;
-    if (add_channel(teams_server, command_line, nb_args, team, channel) == KO)
-        return KO;
-    if (add_thread(teams_server, command_line, nb_args, channel, thread) == KO)
-        return KO;
-    if (add_message(teams_server, command_line, nb_args, thread) == KO)
+    if (add_message(teams_server, command_line, nb_args, create) == KO)
         return KO;
     return OK;
 }
 
 void create_command(teams_server_t *teams_server, char *command)
 {
-    team_t *team = NULL;
-    channel_t *channel = NULL;
-    thread_t *thread = NULL;
+    all_context_t create = {.team = NULL, .channel = NULL, .thread = NULL};
     char **command_line = splitter(command, "\"");
     int nb_args = get_len_char_tab(command_line);
 
@@ -172,9 +139,10 @@ void create_command(teams_server_t *teams_server, char *command)
         dprintf(teams_server->actual_sockfd, SPLITTER_STR);
         return;
     }
-    if (find_all(teams_server, team, channel, thread) == KO)
+    if (find_all(teams_server, create.team, create.channel, create.thread)
+        == KO)
         return;
-    if (add_all(teams_server, command_line, nb_args, team, channel, thread)
+    if (add_all(teams_server, command_line, nb_args, &create)
         == KO)
         return;
 }
---
+++ my_teams_server/src/find_all_context.c
@@ -0,0 +1,77 @@
+/*
+** EPITECH PROJECT, 2024
+** myteams
+** File description:
+** find_all
+*/
+
+
+#include "myteams_server.h"
+
+static int find_team(teams_server_t *teams_server, team_t *team)
+{
+    if (teams_server->clients[teams_server->actual_sockfd].user->
+        team_context[0] != '\0') {
+        team = search_in_teams(&teams_server->all_teams, teams_server->
+        clients[teams_server->actual_sockfd].user->team_context);
+        if (team == NULL) {
+            dprintf(teams_server->actual_sockfd, "504| Unknow team \n");
+            dprintf(teams_server->actual_sockfd, teams_server->clients
+                [teams_server->actual_sockfd].user->team_context);
+            dprintf(teams_server->actual_sockfd, "\n");
+            dprintf(teams_server->actual_sockfd, SPLITTER_STR);
+            return KO;
+        }
+    }
+    return OK;
+}
+
+static int find_channel(teams_server_t *teams_server, team_t *team,
+    channel_t *channel)
+{
+    if (teams_server->clients[teams_server->actual_sockfd].user->
+        channel_context[0] != '\0') {
+        channel = search_in_channels(&team->channels_head, teams_server->
+        clients[teams_server->actual_sockfd].user->channel_context);
+        if (channel == NULL) {
+            dprintf(teams_server->actual_sockfd, "505|Unknown channel ");
+            dprintf(teams_server->actual_sockfd, teams_server->clients
+                [teams_server->actual_sockfd].user->channel_context);
+            dprintf(teams_server->actual_sockfd, "\n");
+            dprintf(teams_server->actual_sockfd, SPLITTER_STR);
+            return KO;
+        }
+    }
+    return OK;
+}
+
+static int find_thread(teams_server_t *teams_server, channel_t *channel,
+    thread_t *thread)
+{
+    if (teams_server->clients[teams_server->actual_sockfd].user->
+        thread_context[0] != '\0') {
+        thread = search_in_threads(&channel->threads_head, teams_server->
+        clients[teams_server->actual_sockfd].user->thread_context);
+        if (thread == NULL) {
+            dprintf(teams_server->actual_sockfd, "506|Unknown thread ");
+            dprintf(teams_server->actual_sockfd, teams_server->clients
+                [teams_server->actual_sockfd].user->thread_context);
+            dprintf(teams_server->actual_sockfd, "\n");
+            dprintf(teams_server->actual_sockfd, SPLITTER_STR);
+            return KO;
+        }
+    }
+    return OK;
+}
+
+int find_all_context(teams_server_t *teams_server, team_t *team,
+    channel_t *channel, thread_t *thread)
+{
+    if (find_team(teams_server, team) == KO)
+        return KO;
+    if (find_channel(teams_server, team, channel) == KO)
+        return KO;
+    if (find_thread(teams_server, channel, thread) == KO)
+        return KO;
+    return OK;
+}
