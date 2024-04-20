06dfd7be077230111b1ecd55ceae6e5b4ddab313
Tom Mendy <tom.mendy@epitech.eu>
2024-04-20 20:21:51 +0200

    [FIX] coding style

--- my_teams_cli/src/myteams_cli.c
+++ my_teams_cli/src/myteams_cli.c
@@ -117,23 +117,21 @@ static int get_client_input_write(fd_set readfds, int socketfd)
     char *str_v = NULL;
     char input[MAX_COMMAND_LENGTH];
 
-    if (FD_ISSET(STDIN_FILENO, &readfds)) {
-        if (fgets(input, MAX_COMMAND_LENGTH, stdin) == NULL)
-            return KO;
-        len = strlen(input);
-        if (len > 0 && input[len - 1] == '\n')
-            input[len - 1] = *END_STR;
-        if (do_error_handling(input) == KO) {
-            printf("\n");
-            return KO;
-        }
-        str_v = add_v_to_str(input);
-        if (write(socketfd, str_v, strlen(str_v) + 1) == -1) {
-            free(str_v);
-            exit(84);
-        }
-        free(str_v);
+    if (fgets(input, MAX_COMMAND_LENGTH, stdin) == NULL)
+        return KO;
+    len = strlen(input);
+    if (len > 0 && input[len - 1] == '\n')
+        input[len - 1] = *END_STR;
+    if (do_error_handling(input) == KO) {
+        printf("\n");
+        return KO;
     }
+    str_v = add_v_to_str(input);
+    if (write(socketfd, str_v, strlen(str_v) + 1) == -1) {
+        free(str_v);
+        exit(84);
+    }
+    free(str_v);
     return OK;
 }
 
