12a8eea41b4a7f23bb5acbea1618352953f8f9a5
Alea Chlodnik <achlodnik@gmail.com>
2024-04-20 17:37:12 +0200

    [UPD] added all create functions minus the reply one, updated the /create comment_body protocol lines

---
+++ my_teams_cli/src/handle_create_cmds.c
@@ -0,0 +1,63 @@
+/*
+** EPITECH PROJECT, 2024
+** myteams
+** File description:
+** handle_create_cmds
+*/
+
+#include "myteams_cli.h"
+
+void create_team(char **info)
+{
+    char *team_uuid = get_msg_up_to_char(info[2], SPLIT_LINE, 0);
+    char *team_name = get_msg_up_to_char(info[2], SPLIT_LINE,
+    (int)strlen(team_uuid) + 1);
+    char *team_desc = get_msg_up_to_char(info[2], END_LINE,
+    (int)strlen(team_uuid) + (int)strlen(team_name) + 2);
+
+    client_event_team_created(team_uuid, team_name, team_desc); ////// every logged user must receive this event ??????
+    client_print_team_created(team_uuid, team_name, team_desc);
+
+    do_multiple_frees(team_uuid, team_name, team_desc, NULL);
+}
+
+void create_channel(char **info)
+{
+    char *channel_uuid = get_msg_up_to_char(info[2], SPLIT_LINE, 0);
+    char *channel_name = get_msg_up_to_char(info[2], SPLIT_LINE,
+    (int)strlen(channel_uuid) + 1);
+    char *channel_desc = get_msg_after_nb(info[2],
+    (int)strlen(channel_uuid) + (int)strlen(channel_name) + 2);
+
+    client_event_channel_created(channel_uuid, channel_name, channel_desc);
+    client_print_channel_created(channel_uuid, channel_name, channel_desc);
+    do_multiple_frees(channel_uuid, channel_name, channel_desc, NULL);
+}
+
+void create_thread(char **info)
+{
+    char *user_uuid = NULL;
+    char *thread_timestamp = NULL;
+    char *thread_title = NULL;
+    char *thread_body = NULL;
+    char *thread_uuid = get_msg_up_to_char(info[2], SPLIT_LINE, 0);
+    int add = (int)strlen(thread_uuid) + 1;
+
+    user_uuid = get_msg_up_to_char(info[2], SPLIT_LINE, add);
+    add += (int)strlen(user_uuid) + 1;
+    thread_timestamp = get_msg_up_to_char(info[2], SPLIT_LINE, add);
+    add += (int)strlen(thread_timestamp) + 1;
+    thread_title = get_msg_up_to_char(info[2], SPLIT_LINE, add);
+    add += (int)strlen(thread_title) + 1;
+    thread_body = get_msg_after_nb(info[2], add);
+    client_event_thread_created(thread_uuid, user_uuid, thread_timestamp,
+    thread_title, thread_body);
+    client_print_thread_created(thread_uuid, user_uuid, thread_timestamp,
+    thread_title, thread_body);
+    do_multiple_frees(thread_uuid, user_uuid, thread_timestamp, thread_title);
+    free(thread_body);
+}
+
+void create_reply(char **info)
+{
+}
