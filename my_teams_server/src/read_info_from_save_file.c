6f16d864a773783bf9123b124fa0256656f27c15
Tom Mendy <tom.mendy@epitech.eu>
2024-04-21 14:11:35 +0200

    [ADD] read team

--- my_teams_server/src/read_info_from_save_file.c
+++ my_teams_server/src/read_info_from_save_file.c
@@ -10,14 +10,14 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int add_user(teams_server_t *teams_server, int file)
+static int add_save_user(teams_server_t *teams_server, int file)
 {
     user_t *user1 = calloc(sizeof(user_t), 1);
 
     if (read(file, user1, sizeof(user1->username) + sizeof(user1->uuid) +
-            sizeof(user1->next) + sizeof(user1->team_context) +
-            sizeof(user1->channel_context) + sizeof(user1->thread_context)
-            + sizeof(user1->nb_clients)) == -1)
+        sizeof(user1->next) + sizeof(user1->team_context) +
+        sizeof(user1->channel_context) + sizeof(user1->thread_context)
+        + sizeof(user1->nb_clients)) == -1)
         return KO;
     if (user1->username[0] == '\0' || user1->uuid[0] == '\0') {
         free(user1);
@@ -32,14 +32,14 @@ int add_user(teams_server_t *teams_server, int file)
     return OK;
 }
 
-int add_private_message(teams_server_t *teams_server, int file)
+static int add_save_private_message(teams_server_t *teams_server, int file)
 {
     message_t *private_message = calloc(sizeof(message_t), 1);
 
     if (read(file, private_message, sizeof(private_message->sender_uuid) +
-            sizeof(private_message->receiver_uuid) +
-            sizeof(private_message->text) + sizeof(private_message->next) +
-            sizeof(private_message->timestamp)) == -1)
+        sizeof(private_message->receiver_uuid) +
+        sizeof(private_message->text) + sizeof(private_message->next) +
+        sizeof(private_message->timestamp)) == -1)
         return KO;
     if (private_message->sender_uuid[0] == '\0' ||
         private_message->receiver_uuid[0] == '\0') {
@@ -51,12 +51,12 @@ int add_private_message(teams_server_t *teams_server, int file)
     return OK;
 }
 
-int add_subscribe(teams_server_t *teams_server, int file)
+static int add_save_subscribe(teams_server_t *teams_server, int file)
 {
     subscribed_t *subscribe = calloc(sizeof(subscribed_t), 1);
 
     if (read(file, subscribe, sizeof(subscribe->team_uuid) +
-            sizeof(subscribe->user_uuid) + sizeof(subscribe->next)) == -1)
+        sizeof(subscribe->user_uuid) + sizeof(subscribe->next)) == -1)
         return KO;
     if (subscribe->user_uuid[0] == '\0' || subscribe->team_uuid[0] == '\0') {
         free(subscribe);
@@ -67,20 +67,57 @@ int add_subscribe(teams_server_t *teams_server, int file)
     return OK;
 }
 
+static int add_save_team(teams_server_t *teams_server, int file)
+{
+    team_t *new_team = calloc(sizeof(team_t), 1);
+
+    if (read(file, new_team, sizeof(new_team->team_uuid) +
+        sizeof(new_team->name) + sizeof(new_team->desc) +
+        sizeof(new_team->next) + sizeof(new_team->channels_head)) == -1)
+        return KO;
+    if (new_team->team_uuid[0] == '\0') {
+        free(new_team);
+    } else {
+        TAILQ_INSERT_TAIL(&teams_server->all_teams, new_team, next);
+    }
+    return OK;
+}
+
+static int add_save_channel(teams_server_t *teams_server, int file)
+{
+    channel_t *new_channel = calloc(sizeof(channel_t), 1);
+
+    if (read(file, new_channel, sizeof(new_channel->channel_uuid) +
+        sizeof(new_channel->name) + sizeof(new_channel->desc) +
+        sizeof(new_channel->next) + sizeof(new_channel->team_uuid)
+        + sizeof(new_channel->threads_head)) == -1)
+        return KO;
+    if (new_channel->channel_uuid[0] == '\0') {
+        free(new_channel);
+    } else {
+        team_t *team = get_team_by_uuid(teams_server, new_channel->team_uuid);
+        TAILQ_INSERT_TAIL(&team->channels_head, new_channel, next);
+    }
+    return OK;
+}
+
 int choose_elem(teams_server_t *teams_server, int file, char delimiter)
 {
     switch (delimiter) {
     case USERS_CHAR:
-        if (add_user(teams_server, file) == KO)
-            return KO;
+        add_save_user(teams_server, file);
         break;
     case PRIVATE_MESSAGE_CHAR:
-        if (add_private_message(teams_server, file) == KO)
-            return KO;
+        add_save_private_message(teams_server, file);
         break;
     case SUBSCRIBE_CHAR:
-        if (add_subscribe(teams_server, file) == KO)
-            return KO;
+        add_save_subscribe(teams_server, file);
+        break;
+    case TEAMS_CHAR:
+        add_save_team(teams_server, file);
+        break;
+    case CHANNELS_CHAR:
+        add_save_channel(teams_server, file);
         break;
     default:
         break;
