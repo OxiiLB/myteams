6d6db4ebfc168f5aa71247fcfd7840f0587faaaa
Tom Mendy <tom.mendy@epitech.eu>
2024-04-21 16:41:23 +0200

    [UPD] read save

--- my_teams_server/src/save_info_to_file.c
+++ my_teams_server/src/save_info_to_file.c
@@ -18,10 +18,7 @@ static void save_users(struct userhead *user_head, int file, char *str)
     TAILQ_FOREACH(new_user, user_head, next) {
         if (new_user->username[0] != 0) {
             write(file, str, sizeof(USERS_CHAR));
-            write(file, new_user, sizeof(new_user->username) +
-            sizeof(new_user->uuid) + sizeof(new_user->next) +
-            sizeof(new_user->team_context) + sizeof(new_user->channel_context)
-            + sizeof(new_user->thread_context) + sizeof(new_user->nb_clients));
+            write(file, new_user, sizeof(user_t));
         }
     }
 }
@@ -35,10 +32,7 @@ static void save_privates_messages(struct messagehead *message_head, int file,
     TAILQ_FOREACH(new_message, message_head, next) {
         if (new_message->text[0] != 0) {
             write(file, str, sizeof(PRIVATE_MESSAGE_CHAR));
-            write(file, new_message, sizeof(new_message->message_uuid) +
-                sizeof(new_message->text) + sizeof(new_message->sender_uuid)
-                + sizeof(new_message->receiver_uuid) +
-                sizeof(new_message->timestamp) + sizeof(new_message->next));
+            write(file, new_message, sizeof(message_t));
         }
     }
 }
@@ -52,9 +46,7 @@ static void save_subscribed(struct subscribedhead *subscribe_head, int file,
     TAILQ_FOREACH(new_subscribe, subscribe_head, next) {
         if (new_subscribe->user_uuid[0] != 0) {
             write(file, str, sizeof(SUBSCRIBE_CHAR));
-            write(file, new_subscribe, sizeof(new_subscribe->team_uuid) +
-                sizeof(new_subscribe->user_uuid) +
-                sizeof(new_subscribe->next));
+            write(file, new_subscribe, sizeof(subscribed_t));
         }
     }
 }
@@ -68,10 +60,7 @@ static void save_reply(struct replyhead *reply_head, int file,
     TAILQ_FOREACH(new_reply, reply_head, next) {
         if (new_reply->reply_uuid[0] != 0) {
             write(file, str, sizeof(REPLY_CHAR));
-            write(file, new_reply, sizeof(new_reply->reply_uuid) +
-                sizeof(new_reply->sender_uuid) + sizeof(new_reply->text)
-                + sizeof(new_reply->next) + sizeof(new_reply->thread_uuid) +
-                sizeof(new_reply->timestamp));
+            write(file, new_reply, sizeof(reply_t));
         }
     }
 }
@@ -85,11 +74,7 @@ static void save_thread(struct threadhead *thread_head, int file,
     TAILQ_FOREACH(new_thread, thread_head, next) {
         if (new_thread->thread_uuid[0] != 0) {
             write(file, str, sizeof(THREADS_CHAR));
-            write(file, new_thread, sizeof(new_thread->thread_uuid) +
-                sizeof(new_thread->title) + sizeof(new_thread->body)
-                + sizeof(new_thread->next) + sizeof(new_thread->replys_head)
-                + sizeof(new_thread->channel_uuid) +
-                sizeof(new_thread->timestamp));
+            write(file, new_thread, sizeof(thread_t));
             save_reply(&new_thread->replys_head, file, str);
         }
     }
@@ -104,10 +89,7 @@ static void save_channel(struct channelhead *channel_head, int file,
     TAILQ_FOREACH(new_channel, channel_head, next) {
         if (new_channel->channel_uuid[0] != 0) {
             write(file, str, sizeof(CHANNELS_CHAR));
-            write(file, new_channel, sizeof(new_channel->channel_uuid) +
-                sizeof(new_channel->name) + sizeof(new_channel->desc)
-                + sizeof(new_channel->next) + sizeof(new_channel->threads_head)
-                + sizeof(new_channel->team_uuid));
+            write(file, new_channel, sizeof(channel_t));
             save_thread(&new_channel->threads_head, file, str);
         }
     }
@@ -122,9 +104,7 @@ static void save_team(struct teamhead *teams_head, int file,
     TAILQ_FOREACH(new_team, teams_head, next) {
         if (new_team->team_uuid[0] != 0) {
             write(file, str, sizeof(TEAMS_CHAR));
-            write(file, new_team, sizeof(new_team->team_uuid) +
-                sizeof(new_team->name) + sizeof(new_team->desc)
-                + sizeof(new_team->next) + sizeof(new_team->channels_head));
+            write(file, new_team, sizeof(team_t));
         }
         save_channel(&new_team->channels_head, file, str);
     }
