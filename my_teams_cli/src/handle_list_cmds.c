6d01b7c37639092b0fede923db5b62ee45634761
Tom Mendy <tom.mendy@epitech.eu>
2024-04-21 20:59:02 +0200

    [FIX] list

--- my_teams_cli/src/handle_list_cmds.c
+++ my_teams_cli/src/handle_list_cmds.c
@@ -17,10 +17,10 @@ void list_teams(char **info, int __attribute__((unused)) add)
     char *team_desc = NULL;
 
     for (int i = 2; info[i] != NULL; i++) {
-        team_uuid = get_msg_up_to_char(info[2], *SPLIT_LINE, 0);
-        team_name = get_msg_up_to_char(info[2], *SPLIT_LINE,
+        team_uuid = get_msg_up_to_char(info[i], *SPLIT_LINE, 0);
+        team_name = get_msg_up_to_char(info[i], *SPLIT_LINE,
         (int)strlen(team_uuid) + 1);
-        team_desc = get_msg_after_nb(info[2],
+        team_desc = get_msg_after_nb(info[i],
         (int)strlen(team_uuid) + (int)strlen(team_name) + 2);
         client_print_teams(team_uuid, team_name, team_desc);
         do_multiple_frees(team_uuid, team_name, team_desc, NULL);
@@ -34,10 +34,10 @@ void list_channels(char **info, int __attribute__((unused)) add)
     char *channel_desc = NULL;
 
     for (int i = 2; info[i] != NULL; i++) {
-        channel_uuid = get_msg_up_to_char(info[2], *SPLIT_LINE, 0);
-        channel_name = get_msg_up_to_char(info[2], *SPLIT_LINE,
+        channel_uuid = get_msg_up_to_char(info[i], *SPLIT_LINE, 0);
+        channel_name = get_msg_up_to_char(info[i], *SPLIT_LINE,
         (int)strlen(channel_uuid) + 1);
-        channel_desc = get_msg_after_nb(info[2],
+        channel_desc = get_msg_after_nb(info[i],
         (int)strlen(channel_uuid) + (int)strlen(channel_name) + 2);
         client_team_print_channels(channel_uuid, channel_name, channel_desc);
         do_multiple_frees(channel_uuid, channel_name, channel_desc, NULL);
@@ -54,14 +54,14 @@ void list_threads(char **info, int add)
 
     for (int i = 2; info[i] != NULL; i++) {
         add = 0;
-        th_uu = get_msg_up_to_char(info[2], *SPLIT_LINE, 0);
+        th_uu = get_msg_up_to_char(info[i], *SPLIT_LINE, 0);
         add = (int)strlen(th_uu) + 1;
-        u_uu = get_msg_up_to_char(info[2], *SPLIT_LINE, add);
+        u_uu = get_msg_up_to_char(info[i], *SPLIT_LINE, add);
         add += (int)strlen(u_uu) + 1;
-        th_ts = get_msg_up_to_char(info[2], *SPLIT_LINE, add);
+        th_ts = get_msg_up_to_char(info[i], *SPLIT_LINE, add);
         add += (int)strlen(th_ts) + 1;
-        th_t = get_msg_up_to_char(info[2], *SPLIT_LINE, add);
-        th_b = get_msg_after_nb(info[2], add + (int)strlen(th_t) + 1);
+        th_t = get_msg_up_to_char(info[i], *SPLIT_LINE, add);
+        th_b = get_msg_after_nb(info[i], add + (int)strlen(th_t) + 1);
         client_channel_print_threads(th_uu, u_uu, (time_t)th_ts, th_t, th_b);
         do_multiple_frees(th_uu, u_uu, th_ts, th_t);
         free(th_b);
@@ -76,12 +76,12 @@ void list_replies(char **info, int __attribute__((unused)) add)
     char *reply_body = NULL;
 
     for (int i = 2; info[i] != NULL; i++) {
-        thread_uuid = get_msg_up_to_char(info[2], *SPLIT_LINE, 0);
-        user_uuid = get_msg_up_to_char(info[2], *SPLIT_LINE,
+        thread_uuid = get_msg_up_to_char(info[i], *SPLIT_LINE, 0);
+        user_uuid = get_msg_up_to_char(info[i], *SPLIT_LINE,
         (int)strlen(thread_uuid) + 1);
-        reply_timestamp = get_msg_up_to_char(info[2], *SPLIT_LINE,
+        reply_timestamp = get_msg_up_to_char(info[i], *SPLIT_LINE,
         (int)strlen(thread_uuid) + (int)strlen(user_uuid) + 2);
-        reply_body = get_msg_after_nb(info[2], (int)strlen(thread_uuid)
+        reply_body = get_msg_after_nb(info[i], (int)strlen(thread_uuid)
         + (int)strlen(user_uuid) + (int)strlen(reply_timestamp) + 3);
         client_thread_print_replies(thread_uuid, user_uuid,
         (time_t)reply_timestamp, reply_body);
